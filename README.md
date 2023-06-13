0531a6f206394874c021eaf475aa49de0d2dc42e
Muhammad Irfan <irfan@jojonomic.com>
2023-06-13 21:37:27 +0700

    init demo 01

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Belajar VueJS
 
 
-
 | Materi | Nama Branch  | 
 | :-------- | :------- | 
 | Reactive Data, Template Syntax, dan Attribute Binding | demo/01 |
@@ -14,4 +13,28 @@
 | Basic component | demo/08 |
 
 
+## Recommended IDE Setup
 
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <main>
+    <!-- Bentuk paling dasar dari pengikatan data adalah interpolasi 
+    teks menggunakan sintaks "Mustache" (kurung kurawal ganda) -->
+    {{ msg }}
+
+    <br>
+    <!-- "Mustache" tidak dapat digunakan di dalam atribut HTML 
+    untuk itu gunakan v-bind atau cukup dengan :[attribute] -->
+    <button v-bind:disabled="isDisabledButton" @click="changeMsg()">Attibute Bind v-bind!</button>
+    or
+    <button :disabled="isDisabledButton" @click="changeMsg()">Attibute Bind Shorthand!</button>
+  </main>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      msg: "Hallo world",
+      isDisabledButton: true
+    }
+  },
+  methods: {
+    changeMsg() {
+      // value msg akan berganti secara REAKTIF. saat terjadi perubahan tampilan juga diperbarui
+      this.msg = "Hi! My Name Is ...."
+    }
+  }
+}
+
+</script>
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
